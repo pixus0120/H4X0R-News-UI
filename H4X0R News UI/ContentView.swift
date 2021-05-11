424a5e5a5216f3d820a67379e3dc26509f3443fe
pixus <pix_us@mail.ru>
2021-05-11 19:36:30 -0400

    Initial Commit

---
+++ H4X0R News UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News UI
+//
+//  Created by locussigilli on 5/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4X0R News UI/H4X0R_News_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_News_UIApp.swift
+//  H4X0R News UI
+//
+//  Created by locussigilli on 5/11/21.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_News_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
